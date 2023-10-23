537dc662792ffcbd19609d6124f6076d3cde5ed6
armandwipangestu <armandwi.pangestu7@gmail.com>
2023-10-24 01:47:38 +0700

    feat: init

---
+++ posts/test.md
@@ -0,0 +1,21 @@
+---
+title: "Test"
+tag: "Linux"
+date: "January 24 2023"
+excerpt: "Pada artikel ini kita akan melakukan configurasi apache untuk enable .htaccess"
+cover_image: "/images/posts/default.png"
+author_name: "Arman Dwi Pangestu"
+author_title: "Web Developer"
+author_image: "/images/posts/author/arman.png"
+---
+
+```bash
+#!/usr/bin/env bash
+
+function main() {
+    text=$1
+    echo $text &> /dev/null
+}
+
+main "haters"
+```
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        primary: "#31B5D5",
+        secondary: "#64748b",
+        dark: "#010613",
+        white: "#ffffff",
+        "accents-100": "#111",
+        "accents-200": "#333",
+        "accents-300": "#444",
+        "accents-400": "#666",
+        "accents-500": "#888",
+        "accents-600": "#999",
+        "accents-700": "#eaeaea",
+        "accents-800": "#fafafa",
+        "emphasis-200": "#ebedf0",
+        "emphasis-gray-600": "#8d949e",
+        "emphasis-gray-700": "#606770",
+        "emphasis-gray-800": "#444950",
+      },
+      screens: {
+        "2xl": "1320px",
       },
     },
   },
   plugins: [],
-}
+};
