f289d88fcfbe88b9e39334d530e90c5ce67eaae2
armandwipangestu <armandwi.pangestu7@gmail.com>
2023-10-24 15:57:42 +0700

    feat: add education page

---
+++ pages/pendidikan.js
@@ -0,0 +1,73 @@
+import Metadata from "@/components/utilities/metadata";
+
+const EducationPage = () => {
+  return (
+    <>
+      <Metadata
+        title="Portofolio - Pendidikan"
+        description="Berikut adalah pendidikan yang saya tempuh"
+        image="/metadata/root.png"
+        url="https://armandwipangestu.vercel.app/skills"
+      />
+
+      <section
+        id="pendidikan"
+        className="pb-32 pt-36 transition-all duration-300 dark:bg-dark"
+      >
+        <div className="container">
+          <div className="w-full px-4">
+            <div className="mx-auto mb-16 text-center">
+              <h4 className="mb-2 text-lg font-semibold uppercase text-primary">
+                Pendidikan
+              </h4>
+            </div>
+          </div>
+
+          <div className="w-full px-4">
+            <div className="flex flex-wrap items-center justify-center">
+              <ol className="border-l border-dark dark:border-white">
+                {/* First Item */}
+                <li>
+                  <div className="flex-start flex items-center pt-3">
+                    <div className="-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-primary"></div>
+                    <p className="inline-flex items-center rounded-full border border-cyan-500/40 bg-cyan-500/10 px-3 py-1 text-xs font-medium tracking-tight text-cyan-500 transition-colors hover:bg-cyan-500/20 focus:outline-none">
+                      2018 - 2021
+                    </p>
+                  </div>
+                  <div className="mb-6 ml-4 mt-2">
+                    <h4 className="tex-dark mb-1.5 text-xl font-semibold dark:text-white">
+                      SMK Merdeka Bandung
+                    </h4>
+                    <p className="mb-3 text-neutral-500 dark:text-neutral-300">
+                      TKJ (Teknik Komputer dan Jaringan)
+                    </p>
+                  </div>
+                </li>
+
+                {/* Second Item */}
+                <li>
+                  <div className="flex-start flex items-center pt-3">
+                    <div className="-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-primary"></div>
+                    <p className="inline-flex items-center rounded-full border border-cyan-500/40 bg-cyan-500/10 px-3 py-1 text-xs font-medium tracking-tight text-cyan-500 transition-colors hover:bg-cyan-500/20 focus:outline-none">
+                      2021 - Sekarang
+                    </p>
+                  </div>
+                  <div className="mb-6 ml-4 mt-2">
+                    <h4 className="tex-dark mb-1.5 text-xl font-semibold dark:text-white">
+                      STMIK Bandung
+                    </h4>
+                    <p className="mb-3 text-neutral-500 dark:text-neutral-300">
+                      S1 Teknik Informatika
+                    </p>
+                  </div>
+                </li>
+              </ol>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default EducationPage;
