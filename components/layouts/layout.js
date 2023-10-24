b8e446c8e966ec536bf5c21e7fb96fd6209001c0
armandwipangestu <armandwi.pangestu7@gmail.com>
2023-10-24 16:49:46 +0700

    feat: add poppins fonts

--- components/layouts/layout.js
+++ components/layouts/layout.js
@@ -1,13 +1,21 @@
+import { Poppins } from "next/font/google";
 import Navigation from "./navigation";
 import Footer from "./footer";
 
+const poppins = Poppins({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
 const Layout = ({ children }) => {
   return (
-    <>
+    <div className={poppins.className}>
       <Navigation />
       <main>{children}</main>
       <Footer />
-    </>
+    </div>
   );
 };
 
