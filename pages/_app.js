9dfd41740b2b6a238ee1da02717e52876deafe69
Arman <armandwi.pangestu7@gmail.com>
2023-10-27 22:33:04 +0700

    feat: add Vercel Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import Layout from "@/components/layouts/layout";
 import "@/styles/globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 }
