a0de0082fb8571784bcc1487a49679dc9239e8bc
Arman <armandwi.pangestu7@gmail.com>
2023-10-27 22:22:47 +0700

    feat: add new nextjs api on /api/menu

---
+++ pages/api/menu.js
@@ -0,0 +1,23 @@
+import Cors from "cors";
+
+const cors = Cors({
+  methods: ["GET"],
+});
+
+const handler = async (req, res) => {
+  cors(req, res, async () => {
+    res.status(200).json({ data });
+  });
+};
+
+const data = [
+  { title: "Beranda", target: "/" },
+  { title: "Tentang Saya", target: "/tentang-saya" },
+  { title: "Portofolio", target: "/portofolio" },
+  { title: "Skills", target: "/skills" },
+  { title: "Pendidikan", target: "/pendidikan" },
+  { title: "Pengalaman", target: "/pengalaman" },
+  { title: "Blog", target: "/blog" },
+];
+
+export default handler;
