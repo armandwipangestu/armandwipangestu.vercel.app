f8d2449a79ad9c75d2a83eeca65070411fd6e332
Arman <armandwi.pangestu7@gmail.com>
2023-12-25 05:45:29 +0700

    fix: animation on tooltip theme

--- components/layouts/navigation.js
+++ components/layouts/navigation.js
@@ -187,14 +187,14 @@ const Navigation = () => {
                           {darkMode ? (
                             <>
                               <Sun className="transition duration-300 ease-in-out dark:text-white" />
-                              <span className="absolute top-[4.2rem] -right-6 scale-0 rounded bg-white p-2 text-xs text-dark group-hover:scale-100 w-[6.5rem] whitespace-normal transition duration-200 ease-in-out font-bold">
+                              <span className="absolute top-[4.2rem] -right-6 scale-0 rounded bg-white p-2 text-xs text-dark group-hover:scale-100 whitespace-normal transition duration-100 ease-in-out font-bold">
                                 ✨ Light Mode
                               </span>
                             </>
                           ) : (
                             <>
                               <Moon className="transition duration-300 ease-in-out text-dark" />
-                              <span className="absolute top-[4.2rem] -right-6 scale-0 rounded bg-dark p-2 text-xs text-white group-hover:scale-100 w-[6.5rem] whitespace-normal transition duration-200 ease-in-out font-bold">
+                              <span className="absolute top-[4.2rem] -right-6 scale-0 rounded bg-dark p-2 text-xs text-white group-hover:scale-100 whitespace-normal transition duration-100 ease-in-out font-bold">
                                 ✨ Dark Mode
                               </span>
                             </>
