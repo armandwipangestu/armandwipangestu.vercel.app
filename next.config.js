a3cb148021e0b8b9757ede365b08641b4355c936
armandwipangestu <armandwi.pangestu7@gmail.com>
2026-02-03 19:00:26 +0700

    feat: add enable redirect to new website

--- next.config.js
+++ next.config.js
@@ -5,6 +5,18 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    if (process.env.ENABLE_REDIRECT_TO_NEW_WEBSITE === "true") {
+      return [
+        {
+          source: "/blog/posts/:slug",
+          destination: `${process.env.NEW_WEBSITE_URL}/id/blog/:slug`,
+          permanent: false,
+        },
+      ];
+    }
+    return [];
+  },
 };
 
 module.exports = nextConfig;
