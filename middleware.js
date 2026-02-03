abdb169f45ccdf88699adea8518de90016445e5a
armandwipangestu <armandwi.pangestu7@gmail.com>
2026-02-03 19:32:30 +0700

    fix: typo url 'apliaksi'

--- middleware.js
+++ middleware.js
@@ -1,16 +1,19 @@
 import { NextResponse } from "next/server";
+import { LEGACY_SLUG_MAP } from "./utilities/legacy-slug-map";
 
 export function middleware(request) {
   if (process.env.ENABLE_REDIRECT_TO_NEW_WEBSITE === "true") {
     const { pathname } = request.nextUrl;
 
     if (pathname.startsWith("/blog/posts/")) {
-      const slug = pathname.replace("/blog/posts/", "");
+        const slug = pathname.replace("/blog/posts/", "");
 
-      return NextResponse.redirect(
-        `${process.env.NEW_WEBSITE_URL}/id/blog/${slug}`,
-        307
-      );
+        const fixedSlug = LEGACY_SLUG_MAP[slug] || slug;
+
+        return NextResponse.redirect(
+        `${process.env.NEW_WEBSITE_URL}/id/blog/${fixedSlug}`,
+        LEGACY_SLUG_MAP[slug] ? 308 : 307
+        );
     }
   }
 
---
+++ utilities/legacy-slug-map.js
@@ -0,0 +1,6 @@
+module.exports = {
+  LEGACY_SLUG_MAP: {
+    "github-actions-membangun-ci-pipeline-apliaksi-reactjs":
+      "github-actions-membangun-ci-pipeline-aplikasi-reactjs",
+  },
+};
